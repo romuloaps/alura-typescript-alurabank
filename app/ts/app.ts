adc437d8a6d3938311416ebc95b17d9fb955ee7d
Romulo Augusto <romulomail@gmail.com>
2020-01-31 08:49:10 -0300

    cria uma interface para mapear os dados do JSON retornado pela API

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -2,5 +2,8 @@ import { NegociacaoController } from "./controllers/NegociacaoController";
 
 const controller = new NegociacaoController();
 
-document.querySelector(".form")
-        .addEventListener("submit", controller.adiciona.bind(controller));
+const form: HTMLFormElement = <HTMLFormElement> document.querySelector(".form");
+const buttonImportar: HTMLButtonElement = <HTMLButtonElement> document.querySelector("#button-importar");
+
+if (form) form.addEventListener("submit", controller.adiciona.bind(controller));
+if (buttonImportar) buttonImportar.addEventListener("click", controller.importaDados.bind(controller));
